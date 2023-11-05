ad859dfa887c24db276fe17844cdf120026df64c
Raearn Malig <malig.raearn@gmail.com>
2023-11-05 23:19:30 +0800

    install daisyui

--- frontend/src/pages/Landing.tsx
+++ frontend/src/pages/Landing.tsx
@@ -1,3 +1,29 @@
 export default function Landing() {
-  return <div className="text-3xl font-bold">Landing Page</div>;
+  return (
+    <div className="navbar bg-base-100">
+      <div className="flex-1">
+        <a className="text-xl normal-case btn btn-ghost">daisyUI</a>
+      </div>
+      <div className="flex-none">
+        <ul className="px-1 menu menu-horizontal">
+          <li>
+            <a>Link</a>
+          </li>
+          <li>
+            <details>
+              <summary>Parent</summary>
+              <ul className="p-2 bg-base-100">
+                <li>
+                  <a>Link 1</a>
+                </li>
+                <li>
+                  <a>Link 2</a>
+                </li>
+              </ul>
+            </details>
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
 }
