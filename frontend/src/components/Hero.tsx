c0266fd5b4dc0a0a294876444f8f31defa5a600c
Raearn Malig <malig.raearn@gmail.com>
2023-11-07 02:57:29 +0800

    fix responsiveness and adjust layout

--- frontend/src/components/Hero.tsx
+++ frontend/src/components/Hero.tsx
@@ -7,14 +7,16 @@ export default function Hero() {
   const { isAuthenticated } = useAuth();
   const navigate = useNavigate();
   return (
-    <div className="bg-white hero min-h-[calc(100vh-270px)]">
+    <div className="bg-white hero min-h-[calc(100vh)]">
       <div className="flex-col lg:gap-60 max-w-[100rem] hero-content lg:flex-row-reverse">
         <Lottie
-          className="lg:w-[75rem] md:w-[30rem] sm:w-[25rem]"
+          className="md:w-[30rem] sm:w-[25rem] lg:w-full"
           animationData={heroAnimation}
         />
         <div className="flex flex-col lg:pl-20 lg:items-start sm:items-center">
-          <h1 className="text-5xl font-black text-blue-950">Voting System</h1>
+          <h1 className="text-5xl font-extrabold text-blue-950">
+            Voting System
+          </h1>
           <p className="py-6 text-blue-950 lg:px-0 sm:px-32 sm:text-center lg:text-left">
             Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda
             excepturi exercitationem quasi. In deleniti eaque aut repudiandae et
