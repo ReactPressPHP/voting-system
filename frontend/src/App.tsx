915892879b58df8e5be98c6c6dbc1191b7df6efa
Raearn Malig <malig.raearn@gmail.com>
2023-11-08 19:41:09 +0800

    set up frontend discord integration

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,10 @@
-import AuthLayout from "./pages/AuthLayout";
+import Layout from "./pages/Layout";
 import SigninForm from "./pages/SigninForm";
 import SignupForm from "./pages/SignupForm";
 import { AuthProvider } from "./contexts/AuthContext";
 import Landing from "./pages/Landing";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import ProtectedRoute from "./pages/ProtectedRoute";
 
 function App() {
   return (
@@ -11,10 +12,17 @@ function App() {
       <BrowserRouter>
         <Routes>
           {/* public Routes */}
-          <Route element={<AuthLayout />}>
+          <Route element={<Layout />}>
             <Route index element={<Landing />} />
             <Route path="/sign-in" element={<SigninForm />} />
-            <Route path="/sign-up" element={<SignupForm />} />
+            <Route
+              path="/dashboard"
+              element={
+                <ProtectedRoute>
+                  <p>Dashboard</p>
+                </ProtectedRoute>
+              }
+            />
           </Route>
         </Routes>
       </BrowserRouter>
--- frontend/src/contexts/AuthContext.tsx
+++ frontend/src/contexts/AuthContext.tsx
@@ -1,13 +1,59 @@
-import { createContext, useContext, useState } from "react";
+import {
+  createContext,
+  useContext,
+  useEffect,
+  useReducer,
+  useState,
+} from "react";
+import { useSearchParams } from "react-router-dom";
 
 const AuthContext = createContext();
 
+const authStatus = {
+  user: "",
+  avatar: "",
+  isAuthenticated: false,
+};
+
+function reducer(state, action) {
+  switch (action.type) {
+    case "LOGIN": //placeholder values
+      return {
+        ...state,
+        user: "andite",
+        avatar:
+          "https://cdn.discordapp.com/avatars/316475410605015041/c782f5556c1c3d7a5a8eda82ba334a90.png",
+        isAuthenticated: true,
+      };
+    case "LOGOUT":
+      return { ...state, isAuthenticated: false };
+  }
+}
 function AuthProvider({ children }) {
-  const [isAuthenticated, setIsAuthenticated] = useState(false);
+  const [{ user, avatar, isAuthenticated }, dispatch] = useReducer(
+    reducer,
+    authStatus
+  );
+
+  const [code, setCode] = useState("");
+
+  function handleLogin() {
+    dispatch({ type: "LOGIN" });
+  }
+
+  function handleLogOut() {
+    dispatch({ type: "LOGOUT" });
+  }
+
   return (
     <AuthContext.Provider
       value={{
+        user,
+        avatar,
         isAuthenticated,
+        dispatch,
+        handleLogin,
+        handleLogOut,
       }}
     >
       {children}
