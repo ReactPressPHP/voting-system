9f11f9fe02870f35ec5d864a2d09b28f84bfd058
gabrielleabasolo <gabrielleabasolo@gmail.com>
2023-11-09 22:37:05 +0800

    created voting event

---
+++ backend/app/Models/vote_tally.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class vote_tally extends Model
+{
+    use HasFactory;
+}
