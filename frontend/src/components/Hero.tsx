bb6d46800e6277627ef84d11b8c01b271acffc8c
Raearn Malig <malig.raearn@gmail.com>
2023-11-09 21:53:09 +0800

    remove unnecessary fragment

--- frontend/src/components/Hero.tsx
+++ frontend/src/components/Hero.tsx
@@ -21,32 +21,30 @@ export default function Hero() {
     }
   }, [data]);
   return (
-    <>
-      <div className="bg-white hero min-h-[calc(100vh)]">
-        <div className="flex-col lg:gap-60 max-w-[100rem] hero-content lg:flex-row-reverse">
-          <Lottie
-            className="md:w-[30rem] sm:w-[25rem] lg:w-full"
-            animationData={heroAnimation}
-          />
-          <div className="flex flex-col lg:pl-20 lg:items-start sm:items-center">
-            <h1 className="text-5xl font-extrabold text-blue-950">
-              Voting System
-            </h1>
-            <p className="py-6 text-blue-950 lg:px-0 sm:px-32 sm:text-center lg:text-left">
-              Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda
-              excepturi exercitationem quasi. In deleniti eaque aut repudiandae
-              et a id nisi.
-            </p>
-            <button
-              onClick={() => navigate(isMember ? "/events" : "/sign-in")}
-              className="text-white btn bg-accent hover:bg-second"
-            >
-              View Events
-            </button>
-            {/* {isMember && <Link to="/events">VOTE</Link>} */}
-          </div>
+    <div className="bg-white hero min-h-[calc(100vh)]">
+      <div className="flex-col lg:gap-60 max-w-[100rem] hero-content lg:flex-row-reverse">
+        <Lottie
+          className="md:w-[30rem] sm:w-[25rem] lg:w-full"
+          animationData={heroAnimation}
+        />
+        <div className="flex flex-col lg:pl-20 lg:items-start sm:items-center">
+          <h1 className="text-5xl font-extrabold text-blue-950">
+            Voting System
+          </h1>
+          <p className="py-6 text-blue-950 lg:px-0 sm:px-32 sm:text-center lg:text-left">
+            Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda
+            excepturi exercitationem quasi. In deleniti eaque aut repudiandae et
+            a id nisi.
+          </p>
+          <button
+            onClick={() => navigate(isMember ? "/events" : "/sign-in")}
+            className="text-white btn bg-accent hover:bg-second"
+          >
+            View Events
+          </button>
+          {/* {isMember && <Link to="/events">VOTE</Link>} */}
         </div>
       </div>
-    </>
+    </div>
   );
 }
